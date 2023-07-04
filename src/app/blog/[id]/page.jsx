10b46e5ba7161037d109846c3e7399f0c9d1f4d9
Ohi20 <ashrafohi20@gmail.com>
2023-07-04 13:48:49 +0600

    Blog-Page-Added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,39 @@
+import Image from 'next/image';
 import React from 'react';
+import styles from './page.module.css';
 
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>Content</p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
