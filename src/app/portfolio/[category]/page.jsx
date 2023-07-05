f3b6873cffef20aa618b7663be0d1ce80cafca10
Ohi20 <ashrafohi20@gmail.com>
2023-07-05 13:38:07 +0600

    Portfolio-LocalData-Added

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,58 +1,38 @@
 import React from 'react';
 import styles from './page.module.css';
-import Button from '../../../components/Button/Button';
 import Image from 'next/image';
+import { items } from './data.js';
+import { notFound } from 'next/navigation';
+import Button from '../../../components/Button/Button';
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 
 const Category = ({ params }) => {
-  console.log(params);
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            className={styles.img}
-            alt="image"
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            className={styles.img}
-            alt="image"
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            className={styles.img}
-            alt="image"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
