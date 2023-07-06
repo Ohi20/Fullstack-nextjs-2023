4cb1716d8f29a7bdbe128e01c488e4cf564b8d74
Ohi20 <ashrafohi20@gmail.com>
2023-07-06 22:33:04 +0600

    Credentials-Provider-Added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import connect from 'src/utils/db';
+import User from 'src/models/User';
+import bcrypt from 'bcryptjs';
 
 const handler = NextAuth({
   providers: [
@@ -8,7 +11,39 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    CredentialsProvider({}),
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'credentials',
+
+      async authorize(credentials) {
+        // check user exists
+        await connect();
+
+        try {
+          const user = await User.findOne({
+            email: credentials.email,
+          });
+
+          if (user) {
+            // check password
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error('Wrong Credentials!');
+            }
+          } else {
+            throw new Error('User not found!');
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
   ],
 });
 
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -2,11 +2,52 @@
 import React from 'react';
 import { getProviders, signIn, useSession } from 'next-auth/react';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 const Login = () => {
   return (
     <div className={styles.container}>
-      <button onClick={() => signIn('google')}>Login with Google</button>
+      <h1 className={styles.title}>Welcome Back</h1>
+      <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+        {/* {error && error} */}
+      </form>
+
+      <button
+        // onClick={() => {
+        //   signIn('google');
+        // }}
+        className={styles.button + ' ' + styles.google}
+      >
+        Login with Google
+      </button>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/register">
+        Create new account
+      </Link>
+
+      {/* <button
+        onClick={() => {
+          signIn("github");
+        }}
+        className={styles.button + " " + styles.github}
+      >
+        Login with Github
+      </button> */}
     </div>
   );
 };
